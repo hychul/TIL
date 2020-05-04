0be3742074f415cbe2251d8e7d10d8e92ceeb47a
hychul <hychome@gmail.com>
2020-05-04 18:08:21 +0900

    Update event source example

--- Go/web/14.event-source/main.go
+++ Go/web/14.event-source/main.go
@@ -1,23 +1,64 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
+	"github.com/antage/eventsource"
 	"github.com/gorilla/pat"
 	"github.com/urfave/negroni"
 )
 
+type Message struct {
+	Sender string `json:"sender"`
+	Msg    string `json:"msg"`
+}
+
+var msgCh chan Message
+
+func joinUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("name")
+	broadcastMessage("", fmt.Sprintf("user joined : %s", username))
+}
+
+func leftUserHandler(w http.ResponseWriter, r *http.Request) {
+	username := r.FormValue("name")
+	broadcastMessage("", fmt.Sprintf("user left : %s", username))
+}
+
 func postMessageHandler(w http.ResponseWriter, r *http.Request) {
 	sender := r.FormValue("sender")
 	msg := r.FormValue("msg")
-	log.Println(sender, msg)
+	broadcastMessage(sender, msg)
+}
+
+func broadcastMessage(sender, msg string) {
+	msgCh <- Message{sender, msg}
+}
+
+func processMsgCh(es eventsource.EventSource) {
+	for msg := range msgCh {
+		data, _ := json.Marshal(msg)
+		es.SendEventMessage(string(data), "", strconv.Itoa(time.Now().Nanosecond()))
+	}
 }
 
 func main() {
-	mux := pat.New()
+	msgCh = make(chan Message)
 
+	es := eventsource.New(nil, nil)
+	defer es.Close()
+
+	go processMsgCh(es)
+
+	mux := pat.New()
+	mux.Post("/users", joinUserHandler)
+	mux.Delete("/users", leftUserHandler)
 	mux.Post("/messages", postMessageHandler)
+	mux.Handle("/stream", es)
 
 	n := negroni.Classic()
 	n.UseHandler(mux)
--- Go/web/14.event-source/public/chat.js
+++ Go/web/14.event-source/public/chat.js
@@ -1,11 +1,11 @@
 $(function() {
     if (!window.EventSource) {
-        alert("This browser doesn't support event source")
-        return
+        alert("This browser doesn't support event source");
+        return;
     }
 
-    var $chatlog = $('#chat-log')
-    var $chatmsg = $('#chat-msg')
+    var $chatlog = $('#chat-log');
+    var $chatmsg = $('#chat-msg');
 
     var isBlank = function(string) {
         return string == null || string.trim() === "";
@@ -15,7 +15,7 @@ $(function() {
     while (isBlank(username)) {
         username = prompt("What's user name?");
         if (!isBlank(username)) {
-            $('#user-name').html('<b>' + username + '</b>')
+            $('#user-name').html('<b>' + username + '</b>');
         }
     }
 
@@ -25,7 +25,38 @@ $(function() {
             msg: $chatmsg.val()
         });
 
-        $chatmsg.val("")
-        $chatmsg.focus()
+        $chatmsg.val("");
+        $chatmsg.focus();
+        return false;
     });
+
+    var addMessage = function(data) {
+        var text = "";
+
+        if (!isBlank(data.sender)) {
+            text = '<strong>' + data.sender + ':</strong>';
+        }
+        text += data.msg;
+
+        $chatlog.prepend('<div><span>' + text + '</span></div>');
+    }
+
+    var es = new EventSource('/stream')
+    es.onopen = function(e) {
+        $.post('/users', {
+            name: username
+        })
+    }
+    es.onmessage = function(e) {
+        var msg = JSON.parse(e.data);
+        addMessage(msg);
+    }
+
+    window.onbeforeunload = function() {
+        $.ajax({
+            url: "/users?name=" + username,
+            type: "DELETE"
+        });
+        es.close();
+    }
 })
