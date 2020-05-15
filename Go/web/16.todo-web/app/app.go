4c931e46380446d50966c3b26e97f805735de110
hychul <hychome@gmail.com>
2020-05-15 23:21:19 +0900

    Create new api to get todo list

--- Go/web/16.todo-web/app/app.go
+++ Go/web/16.todo-web/app/app.go
@@ -2,17 +2,44 @@ package app
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
 )
+
+var rd *render.Render
+
+type Todo struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	CreateAt  time.Time `json:"create_at"`
+}
+
+var todoMap map[int]*Todo
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/todo.html", http.StatusTemporaryRedirect)
 }
 
-func MakeHandler() http.Handler {
-	r := mux.NewRouter()
+func getTodoList(w http.ResponseWriter, r *http.Request) {
+	list := []*Todo{}
+	for _, v := range todoMap {
+		list = append(list, v)
+	}
 
-	r.HandleFunc("/", indexHandler)
-	return r
+	rd.JSON(w, http.StatusOK, list)
+}
+
+func MakeHandler() http.Handler {
+	todoMap = make(map[int]*Todo)
+
+	rd = render.New()
+	mux := mux.NewRouter()
+
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/todos", getTodoList).Methods("GET")
+
+	return mux
 }
--- Go/web/16.todo-web/go.mod
+++ Go/web/16.todo-web/go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/gorilla/mux v1.7.4
+	github.com/unrolled/render v1.0.3
 	github.com/urfave/negroni v1.0.0
 )
