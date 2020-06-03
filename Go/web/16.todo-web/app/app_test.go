f28cd4fff2e065dcabd85bf346fc9d63e418c899
hychul <hychome@gmail.com>
2020-06-03 23:06:04 +0900

    Add new test for update todo

--- Go/web/16.todo-web/app/app_test.go
+++ Go/web/16.todo-web/app/app_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"net/url"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -69,6 +70,58 @@ func TestGetTodos(t *testing.T) {
 			assert.Equal(t.Name, "Test todo 1")
 		} else if t.ID == id2 {
 			assert.Equal(t.Name, "Test todo 2")
+		} else {
+			assert.Error(fmt.Errorf("ID MUST BE %d or %d", id1, id2))
+		}
+	}
+}
+
+func TestUpdateTodo(t *testing.T) {
+func TestUpdateTodo(t *testing.T) {
+	// given
+	assert := assert.New(t)
+	ts := httptest.NewServer(MakeHandler())
+
+	defer ts.Close()
+
+	resp, err := http.PostForm(ts.URL+"/todos", url.Values{"name": {"Test todo 1"}})
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+	var todo Todo
+	err = json.NewDecoder(resp.Body).Decode(&todo)
+	assert.NoError(err)
+	assert.Equal(todo.Name, "Test todo 1")
+	id1 := todo.ID
+
+	resp, err = http.PostForm(ts.URL+"/todos", url.Values{"name": {"Test todo 2"}})
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+	err = json.NewDecoder(resp.Body).Decode(&todo)
+	assert.NoError(err)
+	assert.Equal(todo.Name, "Test todo 2")
+	id2 := todo.ID
+
+	// when
+	req, _ := http.NewRequest("PUT", ts.URL+"/todos/"+strconv.Itoa(id1)+"?complete=true", nil)
+	resp, err = http.DefaultClient.Do(req)
+
+	// then
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	resp, err = http.Get(ts.URL + "/todos")
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+	todos := []*Todo{}
+	err = json.NewDecoder(resp.Body).Decode(&todos)
+	assert.NoError(err)
+	assert.Equal(len(todos), 2)
+	for _, t := range todos {
+		if t.ID == id1 {
+			assert.Equal(t.Name, "Test todo 1")
+			assert.Equal(t.Completed, true)
+		} else if t.ID == id2 {
+			assert.Equal(t.Name, "Test todo 2")
+			assert.Equal(t.Completed, false)
 		} else {
 			assert.Error(fmt.Errorf("ID MUST BE %d or %d", id1, id2))
 		}
